4db410d0a7f6cad56e9f30d338c304635978f9da
marbloo <marielle.calasahan@gmail.com>
2024-10-25 03:53:04 +0200

    FirstClassroom sound update

--- ptsd/Assets/Mar/M_Scripts/doorOpen.cs
+++ ptsd/Assets/Mar/M_Scripts/doorOpen.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class doorOpen : MonoBehaviour
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip clip;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Door"))
+        if (other.gameObject.CompareTag("Door") || other.gameObject.CompareTag("Player"))
         {
-
+            audioSource.PlayOneShot(clip);
         }
     }
 
